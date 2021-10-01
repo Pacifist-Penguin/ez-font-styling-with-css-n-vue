08110dcb498bc4021fb3d37989eb082809d5613d
Pacifist-Penguin <7593121+Pacifist-Penguin@users.noreply.github.com>
2021-10-01 23:57:13 +0300

    Text Shadow is kinda here. Have to fix StyleDisplay 4 that tho

--- src/components/TextShadowConfig.vue
+++ src/components/TextShadowConfig.vue
@@ -11,33 +11,68 @@
 export default {
 	name: "TextShadowConfig",
 	emits: {
-		shadowStyle: (shadowStyle) => typeof shadowStyle === "object"
+		styleEmited: (styles) => typeof styles === "string"
 	},
 	props: {
 		shadowCount: {
 			type: Number,
-			required: false,
-			default: 0
+			required: true,
+			default: 1
+		},
+		useVmin: {
+			type: String,
+			required: true,
+			default: "px"
 		}
 	},
-	computed: {},
+	computed: {
+		style() {
+			const arr = [];
+			this.arrayOfShadows.forEach((element, index) => {
+				for (const key in element) {
+					if (Object.hasOwnProperty.call(element, key)) {
+						let string = element[key];
+						if (key === "XOffset" || key === "YOffset" || key === "blurRadius") {
+							string += this.useVmin;
+						}
+						if (key === "color" && this.shadowCount - 1 > index) {
+							string += ",";
+						}
+						arr.push(string);
+					}
+				}
+			});
+			const string = arr.join(" ");
+			return string;
+		}
+	},
 	data() {
 		return {
 			arrayOfShadows: []
 		};
 	},
-	methods: {
-		generateArrayOfShadows() {
-			const arr = [];
-			for (let index = 0; index < this.shadowCount; index++) {
-				arr.push({});
-			}
-			this.arrayOfShadows = arr;
-		}
-	},
 	watch: {
-		shadowCount: function () {
-			this.generateArrayOfShadows();
+		shadowCount: function (state, prevState) {
+			if (state > prevState) {
+				this.arrayOfShadows.push({
+					XOffset: 0,
+					YOffset: 0,
+					blurRadius: 0,
+					color: "#ffffff"
+				});
+			} else {
+				this.arrayOfShadows.pop();
+			}
+		},
+		style: function () {
+			//emit this if there's no shadows
+			if (this.style === "") {
+				this.$emit("styleEmited", "unset");
+			}
+			//else emit styles
+			else {
+				this.$emit("styleEmited", this.style);
+			}
 		}
 	}
 };
